00bc4acc0bbd3fbf78f175a23b93cf8ba92fff50
mohamed gharieb <m.gharieb89@gmail.com>
2024-04-01 14:05:50 -0700

    t4v0

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,19 @@ def service_status():
     """created route returns status of RESTful service
     TODO check if this formatting is okay for json response"""
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def object_stats():
+    """returns the count of object types"""
+
+    classes = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+                                                                }
+    jsonify(classes).status_code = 200
+    return jsonify(classes)
