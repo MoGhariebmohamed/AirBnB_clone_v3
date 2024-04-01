5733da57cc782a9ae5a58565c9af4b84a6d3e837
mohamed gharieb <m.gharieb89@gmail.com>
2024-04-01 15:14:25 -0700

    t3c\v5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
+
 @app_views.route('/status', strict_slashes=False)
 def service_status():
     """created route returns status of RESTful service
