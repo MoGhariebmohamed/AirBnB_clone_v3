acc626dc7e95a0fc63e1ad06908cb471b8ff14c1
Rania FATHALLAH <rania14fathallah@gmail.com>
2024-04-02 20:45:17 +0100

    1

---
+++ api/v1/views/places.py
@@ -0,0 +1,77 @@
+# api/v1/views/places.py
+#!/usr/bin/python3
+"""
+    API for Place objects
+"""
+
+from models.place import Place
+from models.city import City
+from models.user import User
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+
+
+@app_views.route('/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
+def get_places_by_city(city_id):
+    """Retrieves the list of all Place objects of a City"""
+    city = City.get(city_id)
+    if not city:
+        abort(404)
+    places = [place.to_dict() for place in city.places()]
+    return jsonify(places)
+
+
+@app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
+def get_place(place_id):
+    """Retrieves a Place object"""
+    place = Place.get(place_id)
+    if not place:
+        abort(404)
+    return jsonify(place.to_dict())
+
+
+@app_views.route('/places/<place_id>', methods=['DELETE'], strict_slashes=False)
+def delete_place(place_id):
+    """Deletes a Place object"""
+    place = Place.get(place_id)
+    if not place:
+        abort(404)
+    place.delete()
+    return jsonify({}), 200
+
+
+@app_views.route('/cities/<city_id>/places', methods=['POST'], strict_slashes=False)
+def create_place(city_id):
+    """Creates a Place"""
+    city = City.get(city_id)
+    if not city:
+        abort(404)
+    data = request.get_json()
+    if not data:
+        abort(400, 'Not a JSON')
+    if 'user_id' not in data:
+        abort(400, 'Missing user_id')
+    if 'name' not in data:
+        abort(400, 'Missing name')
+    user_id = data['user_id']
+    if not User.get(user_id):
+        abort(404)
+    new_place = Place(city_id=city_id, **data)
+    new_place.save()
+    return jsonify(new_place.to_dict()), 201
+
+
+@app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
+def update_place(place_id):
+    """Updates a Place object"""
+    place = Place.get(place_id)
+    if not place:
+        abort(404)
+    data = request.get_json()
+    if not data:
+        abort(400, 'Not a JSON')
+    for key, value in data.items():
+        if key not in ['id', 'user_id', 'city_id', 'created_at', 'updated_at']:
+            setattr(place, key, value)
+    place.save()
+    return jsonify(place.to_dict()), 200
---
+++ api/v1/views/users.py
@@ -0,0 +1,66 @@
+# api/v1/views/users.py
+#!/usr/bin/python3
+"""
+    API for User objects
+"""
+
+from models.user import User
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+
+
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def get_users():
+    """Retrieves the list of all User objects"""
+    users = [user.to_dict() for user in User.all()]
+    return jsonify(users)
+
+
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+def get_user(user_id):
+    """Retrieves a User object"""
+    user = User.get(user_id)
+    if not user:
+        abort(404)
+    return jsonify(user.to_dict())
+
+
+@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
+def delete_user(user_id):
+    """Deletes a User object"""
+    user = User.get(user_id)
+    if not user:
+        abort(404)
+    user.delete()
+    return jsonify({}), 200
+
+
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def create_user():
+    """Creates a User"""
+    data = request.get_json()
+    if not data:
+        abort(400, 'Not a JSON')
+    if 'email' not in data:
+        abort(400, 'Missing email')
+    if 'password' not in data:
+        abort(400, 'Missing password')
+    new_user = User(**data)
+    new_user.save()
+    return jsonify(new_user.to_dict()), 201
+
+
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+def update_user(user_id):
+    """Updates a User object"""
+    user = User.get(user_id)
+    if not user:
+        abort(404)
+    data = request.get_json()
+    if not data:
+        abort(400, 'Not a JSON')
+    for key, value in data.items():
+        if key not in ['id', 'email', 'created_at', 'updated_at']:
+            setattr(user, key, value)
+    user.save()
+    return jsonify(user.to_dict()), 200
