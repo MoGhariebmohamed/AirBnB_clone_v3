bdbd6ac776e7373db57026cd16d53e2c2e1e3557
Rania FATHALLAH <rania14fathallah@gmail.com>
2024-04-02 20:36:34 +0100

    1

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,12 @@
+# api/v1/views/__init__.py
 #!/usr/bin/python3
-"""the init file"""
+"""
+    Initialize Flask Blueprint object
+"""
 
 from flask import Blueprint
 
-
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
 from api.v1.views.index import *
+from api.v1.views.states import *
